9c4da9105df07c73def85e2c50cd999d4651f538
Marcin Kurczewski <mkurczew@gmail.com>
2014-12-02 21:00:45 +0100

    Improved performance by ~35%

--- lib/tlg5_reader.rb
+++ lib/tlg5_reader.rb
@@ -49,6 +49,7 @@ class Tlg5Reader
   def load_pixel_block_row(channel_data, block_y, output_pixels)
     max_y = block_y + @header.block_height
     max_y = @header.image_height if max_y > @header.image_height
+    use_alpha = @header.channel_count == 4
 
     (block_y..max_y-1).each do |y|
       prev_red = 0
@@ -57,14 +58,13 @@ class Tlg5Reader
       prev_alpha = 0
 
       block_y_shift = (y - block_y) * @header.image_width
-      y_shift = y * @header.image_width
       prev_y_shift = (y - 1) * @header.image_width
 
       (0..@header.image_width - 1).each do |x|
         red = channel_data[2][block_y_shift + x]
         green = channel_data[1][block_y_shift + x]
         blue = channel_data[0][block_y_shift + x]
-        alpha = @header.channel_count == 4 ? channel_data[3][block_y_shift + x] : 0
+        alpha = use_alpha ? channel_data[3][block_y_shift + x] : 0
 
         red += green
         blue += green
@@ -74,22 +74,25 @@ class Tlg5Reader
         prev_blue += blue
         prev_alpha += alpha
 
-        output_pixel = [
-          prev_red,
-          prev_green,
-          prev_blue,
-          prev_alpha
-        ]
+        output_red = prev_red
+        output_green = prev_green
+        output_blue = prev_blue
+        output_alpha = prev_alpha
 
         if y > 0
-          output_pixel[0] += output_pixels[prev_y_shift + x][0]
-          output_pixel[1] += output_pixels[prev_y_shift + x][1]
-          output_pixel[2] += output_pixels[prev_y_shift + x][2]
-          output_pixel[3] += output_pixels[prev_y_shift + x][3]
+          index = (prev_y_shift + x) * 4
+          output_red += output_pixels[index + 0]
+          output_green += output_pixels[index + 1]
+          output_blue += output_pixels[index + 2]
+          output_alpha += output_pixels[index + 3]
         end
 
-        output_pixel[3] = 255 if @header.channel_count == 3
-        output_pixels[y_shift + x] = output_pixel
+        output_alpha = 255 unless use_alpha
+
+        output_pixels << output_red
+        output_pixels << output_green
+        output_pixels << output_blue
+        output_pixels << output_alpha
       end
     end
   end
@@ -105,34 +108,41 @@ class Tlg5Reader
   end
 
   def decompress_block(block_info)
-    input_data = StringIO.new(block_info.block_data.pack('C*'))
+    input_data = block_info.block_data
     output_data = []
     flags = 0
-    while ! input_data.eof?
+    i = 0
+    while i < block_info.block_size
       flags >>= 1
       if (flags & 0x100) != 0x100
-        flags = input_data.read(1).unpack('C')[0] | 0xff00
+        flags = input_data[i] | 0xff00
+        i += 1
       end
 
       if (flags & 1) == 1
-        x0, x1 = input_data.read(2).unpack('CC')
+        x0, x1 = input_data[i], input_data[i + 1]
+        i += 2
         position = x0 | ((x1 & 0xf) << 8)
         length = 3 + ((x1 & 0xf0) >> 4)
         if length == 18
-          length += input_data.read(1).unpack('C')[0]
+          length += input_data[i]
+          i += 1
         end
-        (1..length).each do
+        j = 0
+        while j < length do
           c = @compressor_state.text[position]
-          output_data.push(c)
+          output_data << c
           @compressor_state.text[@compressor_state.offset] = c
           @compressor_state.offset += 1
           @compressor_state.offset &= 0xfff
           position += 1
           position &= 0xfff
+          j += 1
         end
       else
-        c = input_data.read(1).unpack('C')[0]
-        output_data.push(c)
+        c = input_data[i]
+        i += 1
+        output_data << c
         @compressor_state.text[@compressor_state.offset] = c
         @compressor_state.offset += 1
         @compressor_state.offset &= 0xfff
